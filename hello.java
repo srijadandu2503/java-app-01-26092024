10c046eaca0614bcb07442d1a009335fe0b9fd57
Dandu Srija <dandusrija2503@gmail.com>
2024-09-26 16:56:52 +0530

    Added hello.java assigned to me.

---
+++ hello.java
@@ -0,0 +1,16 @@
+class hello{
+	public static void main(String[] args){
+		System.out.println("Hello");
+	}
+}
+class hi{
+	public static void main(String[] args){
+		System.out.println("Hi");
+	}
+}
+class hey{
+	public static void main(String[] args){
+		System.out.println("Hey");
+	}
+}
+	
---
+++ hey.java
@@ -0,0 +1,6 @@
+class hey{
+	public static void main(String[] args){
+		System.out.println("Hey");
+	}
+}
+	
